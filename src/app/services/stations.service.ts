3b1c2fdeabd69f985466a1faad8950d725e09aa1
GowthamF <40605124+GowthamF@users.noreply.github.com>
2021-02-07 21:59:03 +0530

    adding filteration

--- src/app/services/stations.service.ts
+++ src/app/services/stations.service.ts
@@ -29,7 +29,34 @@ export class StationsService {
       .subscribe((data) => {
         data.stations.splice(0, 1);
         data.times.splice(0, 1);
-        this.stations.next(data.stations);
+        //This is temporary. Once Connected Trains API implemented, this filteration will be removed
+        var filteredStation = data.stations.filter((val) => {
+          return (
+            val.id === 23 ||
+            val.id === 24 ||
+            val.id === 44 ||
+            val.id === 61 ||
+            val.id === 54 ||
+            val.id === 57 ||
+            val.id === 68 ||
+            val.id === 75 ||
+            val.id === 96 ||
+            val.id === 86 ||
+            val.id === 400 ||
+            val.id === 148 ||
+            val.id === 147 ||
+            val.id === 115 ||
+            val.id === 142 ||
+            val.id === 187 ||
+            val.id === 184 ||
+            val.id === 220 ||
+            val.id === 235 ||
+            val.id === 246 ||
+            val.id === 289
+          );
+        });
+        // console.log(dd);
+        this.stations.next(filteredStation);
         this.times.next(data.times);
         this.isLoading = false;
       });
